8e0e499044acd86e2f0101b1b2aeb5e7247b6018
Ben Lazan <blazan@umass.edu>
2017-02-05 20:39:16 -0500

    initial folder and database creation

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-# config file for SODA
-# Sensor Organizaing Data Application
+# config.py
+# configuration file for 
+# SODA: Sensor Organizaing Data Application
   
 # name of project or locationwill dictate the name of the
 # folder which will hold data for one activity
-
+# It is recommended to not use spaces in this name.
 PROJECT_NAME = "Amherst"
 
-# the project path is the top level location where
+# the project path is the top level directory where
 # data will be saved in its individual project folder.
 # this folder should already exist. 
 # 
-# **The 'r' in front of the string which holds the path is important!**
-# **you need it or you need fromff
-
+# *** forward slashes in path names must be doubled
+# *** like this -> "C:\\Your\\Path\\Here\\"
 STORAGE_PATH = "C:\\Users\\ben\\test\\"
--- soda.py
+++ soda.py
@@ -7,8 +7,40 @@ from time import sleep
 import sqlite3
 
 
-system('cls')
+# function for creating new project directory
+def newProject():
+    print("Creating new directory " + project_path + ".......", end="")
+    # make project file path
+    try:
+        makedirs(project_path)
+    except Exception as e:
+        print()
+        print("Error: Couldn't create new directory")
+        print(e)
+        sys.exit()
+    print("OK")
 
+# connect to database code is the same whether accessing existing or creating new
+# returns a sqlite3 connection object used to interact with the database
+def connectToDB(path_to_db_file):
+    print("Connecting to database at " + database_path + ".......", end="")
+    try:
+        conn = sqlite3.connect(path_to_db_file)
+    except Exception as e:
+        print()
+        print("Error: Couldn't connect to the database")
+        print(e)
+        sys.exit()
+    print("OK")
+    return conn
+
+def closeDB(conn):
+    conn.commit()
+    conn.close()
+    print("Database closed.")
+
+# Main program entry here
+system('cls')
 
 print("SODA: Sensor Organizing Data Application")
 print()
@@ -32,24 +64,22 @@ database_path = path.join(project_path,database_name)
 print("Configuration settings imported from config.py")
 #sleep(1)
 print("Configuration check:")
-#sleep(1)
 print()
-print("Data storage path: " + storage_path)
-print("Project name: " + project_name)
+print("     Data storage path: " + storage_path)
+print("     Project name: " + project_name)
 print()
 
 projectExists = path.exists(project_path)
-#sleep(1.5)
 
 if (projectExists):
     print("The " + project_name + " project directory already exists at " + project_path)
-    print("If you command you will be importing data into that project.")
+    print("***If you continue you will be importing data into that project folder and database.***")
 else:
     print("Project " + project_name + " does not currently exist")
     print("A new project folder will be created at " + project_path)
     print("A new database file will be created at " + database_path)
 
-sleep(1.5)
+
 print()
 command = ""
 while (command != "yes" and command != "no"):
@@ -58,18 +88,22 @@ while (command != "yes" and command != "no"):
 if (command == "no"):
     print("OK! Quitting...")
     sys.exit()
+
 if (not projectExists):
     newProject()
 
-def newProject():
-    print("Creating new directory " + project_path, end="")
-    try:
-        makedirs(project_path)
-    except Exception as e:
-        print("Error: Couldn't create new directory")
-        print(e)
-        sys.exit()
-    print("Directory created.")
+conn = connectToDB(database_path)
+# a cursor object is used to execute database inserts and queries
+c = conn.cursor()
+
+
+### DATABASE TESTING
+c.execute("""CREATE TABLE IF NOT EXISTS awesomepossum(row_id INTEGER PRIMARY KEY, name text NO NULL)""")
+c.execute("""INSERT INTO awesomepossum VALUES(NULL, "Billy")""")
+print("INSERT BABY")
+closeDB(conn);
+
+
 
 
 
